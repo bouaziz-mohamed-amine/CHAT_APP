37b20204cdc4f708b36d5728bb99270dbe017228
DELL <amine.m.a1998@gmail.com>
2021-02-07 18:25:20 +0100

    first step

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+}
